bb86988badb358410234af94d18db7c66278ed19
sandromiralmeida <120032218+sandromiralmeida@users.noreply.github.com>
2022-12-07 11:36:25 -0300

    Add files via upload

---
+++ _topo.php
@@ -0,0 +1,33 @@
+<div class="container-fluid">
+<div class="container">
+<div class="row">
+<div class="col-xl-12"> <a href="index.php"><img src="imagens/logo-ctec.png" class="img-fluid"></a> </div>
+</div>
+	
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="azul.php">Azul</a>
+      </li>
+		<li class="nav-item">
+        <a class="nav-link" href="verde.php">Verde</a>
+      </li>
+		<li class="nav-item">
+        <a class="nav-link" href="vermelha.php">Vermelha</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>	
+	
+</div>	
+</div>
---
+++ verde.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CTec em PWA</title>
+
+<!-- Metatag responsiva -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+<!-- Ícone -->
+<link rel="short icon" href="favicon.gif">
+
+<link href="bootstrap-4.4.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">	
+	
+</head>
+
+<body style="background-color: #09DD43">
+<?php include("_topo.php"); ?>	
+	
+<!-- Conteúdo -->	
+<div class="container">
+<br>
+<br>
+<br>
+	
+<h1>PWA Verde</h1>	
+
+</div>	
+<!-- FIM Conteúdo -->		
+	
+<!-- JS Bootstrap atualizado de 3.4.1 para 3.6.1 segurança -->
+<script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>	
+
+<!-- JS Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<!-- FIM JS Bootstrap -->	
+	
+	
+</body>
+</html>
